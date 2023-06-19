7cf7d29b5712908c526a9f9e474f8d5a6e97384e
LeeInGyu <dldlsrb1414@gmail.com>
2023-06-19 16:47:39 +0900

    컴퓨터공학과 페이지 주소 연결

--- csecom/locker/admin.py
+++ csecom/locker/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Users)
-admin.site.register(Lockers)
+admin.site.register(Student)
+admin.site.register(Locker)
--- csecom/locker/forms.py
+++ csecom/locker/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 from .models import Lockers
 
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = Lockers
+        fields = ['student_number', 'name', 'password']
+        labels = {
+            'student_number': '학번',
+            'name': '이름',
+            'password': '비밀번호',
+        }
+
 class LockerForm(forms.ModelForm):
     class Meta:
         model = Lockers
